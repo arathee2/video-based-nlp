a2a2676edbb52962970404b62502ab31e6bef868
Amandeep Rathee <arathee2@gmail.com>
2021-04-02 23:50:36 +0000

    add name to html form

--- app/app.py
+++ app/app.py
@@ -4,10 +4,11 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods = ["GET", "POST"])
 def home():
+    DEFAULT_URL = "https://youtu.be/9MHYHgh4jYc"
     if flask.request.method == "POST":
         return flask.render_template("print-url.html", url=flask.request.form['url'])
     else:
-        return flask.render_template("home.html")
+        return flask.render_template("home.html", default_url=DEFAULT_URL)
 
 
 if __name__ == "__main__":
