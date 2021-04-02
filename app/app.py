29d9851e82ab308925f243aaccd27456ad408c25
Amandeep Rathee <arathee2@gmail.com>
2021-04-02 23:40:11 +0000

    try reading form

--- app/app.py
+++ app/app.py
@@ -1,10 +1,15 @@
 import flask
+import requests
 app = flask.Flask(__name__)
 
 @app.route('/', methods = ["GET", "POST"])
 def home():
-    DEFAULT_URL = "https://youtu.be/KE-hrWTgDjk"
-    return flask.render_template("home.html", default_url=DEFAULT_URL)
+    if requests.method == "POST":
+        return flask.render_template("print-url.html", url=requests.form['default_url'])
+    else:
+        DEFAULT_URL = "https://youtu.be/KE-hrWTgDjk"
+        return flask.render_template("home.html", default_url=DEFAULT_URL)
+
 
 if __name__ == "__main__":
     app.run()
