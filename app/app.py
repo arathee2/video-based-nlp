d3d5e1cbd33783134c6d61b78508efddbb5f3e23
EC2 Default User <ec2-user@ip-172-31-40-175.us-east-2.compute.internal>
2021-04-21 03:25:41 +0000

    add functionality to download youtube mp3 to s3

--- app/app.py
+++ app/app.py
@@ -1,17 +1,18 @@
+import glob
 import flask
 import youtube_dl
+import boto3
 
-app = flask.Flask(__name__)
-
-@app.route('/', methods = ["GET", "POST"])
-def home():
-    DEFAULT_URL = "https://youtu.be/9MHYHgh4jYc"
-    if flask.request.method == "POST":
-        youtube_download(flask.request.form['url'])
-        return "<h3> Video downloaded successfully to root directory.</h3>"
-    else:
-        return flask.render_template("home.html", default_url=DEFAULT_URL)
-
+def get_filenames(directory, extension='mp3'):
+    """ 
+    Return all filenames in a directory for an arbitrary extension.
+    """
+    filenames = []
+    os.chdir(directory)
+    for file in glob.glob(f"*.{extension}"):
+        filenames.append(file)
+    return filenames
+    
 def youtube_download(url):
     def my_hook(d):
         if d['status'] == 'finished':
@@ -28,6 +29,49 @@ def youtube_download(url):
     
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         #ydl.download([url])
+
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3 = boto3.client('s3')
+    with open(file_name, "rb") as f:
+        s3.upload_fileobj(f, bucket, object_name)
+
+    return True
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods = ["GET", "POST"])
+def home():
+    DEFAULT_URL = "https://youtu.be/9MHYHgh4jYc"
+    if flask.request.method == "POST":
+        # download mp3 to project root directory i.e. same level as `app`.
+        youtube_download(flask.request.form['url'])
         
+        # upload all mp3 files to s3
+        directory = "../"
+        extension = 'mp3'
+        filenames = get_filenames(directory, extension)
+        bucket = 'audio-from-video-based-nlp'
+        for filename in filenames:
+            success = upload_file(filename, bucket)
+        if success:
+            return f"<h3> Video uploaded successfully to s3 bucket: {bucket}.</h3>"
+        else:
+            return f"<h3> Video NOT uploaded successfully to s3 bucket: {bucket}.</h3>"
+    else:
+        return flask.render_template("home.html", default_url=DEFAULT_URL)
+
 if __name__ == "__main__":
     app.run()
